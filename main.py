a8a823ca114a977ff83dd4752f219220bf415c17
V <71629856+bombermon@users.noreply.github.com>
2020-10-23 23:42:43 +0300

    near final but need to check form of inputa

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-#ИМПОРТ БИБЛИОТЕК
+# ИМПОРТ БИБЛИОТЕК
 import re
 from num2words import num2words
-#КОНЕЦ ИМПОРТА БИБЛИОТЕК
 
-#НАЧАЛО РАБОТЫ С ДАННЫМИ
-digits_dict = {'ноль': 0,'один': 1,'два': 2,'три': 3, 'четыре': 4, 'пять': 5, 'шесть': 6, 'семь': 7,'восемь'
-                  :8, 'девять': 9,'десять':10, 'одиннадцать': 11, 'двенадцать': 12, 'тринадцать': 13, 'четырнадцать':
-                  14,'пятнадцать': 15, 'шестнадцать': 16, 'семнадцать': 17, 'восемнадцать': 18, 'девятнадцать':19,
-                  'двадцать': 20,'тридцать': 30,'сорок': 40,'пятьдесят': 50, 'шестьдесят': 60, 'семьдесят': 70, 'восемьдесят':
-                  80, 'девяносто': 90}
+# КОНЕЦ ИМПОРТА БИБЛИОТЕК
+
+# НАЧАЛО РАБОТЫ С ДАННЫМИ
+digits_dict = {'ноль': 0, 'один': 1, 'два': 2, 'три': 3, 'четыре': 4, 'пять': 5, 'шесть': 6, 'семь': 7, 'восемь'
+: 8, 'девять': 9, 'десять': 10, 'одиннадцать': 11, 'двенадцать': 12, 'тринадцать': 13, 'четырнадцать':
+                   14, 'пятнадцать': 15, 'шестнадцать': 16, 'семнадцать': 17, 'восемнадцать': 18, 'девятнадцать': 19,
+               'двадцать': 20, 'тридцать': 30, 'сорок': 40, 'пятьдесят': 50, 'шестьдесят': 60, 'семьдесят': 70,
+               'восемьдесят':
+                   80, 'девяносто': 90}
 operation_dict = ['плюс', 'минус', 'умножить', 'разделить на']
 
 adot_dict = {'одна десятая': 0.1, 'одна сотая': 0.01, 'одна тысячная': 0.001, 'одна десятитысячная': 0.0001,
              'одна стотысячная': 0.00001, 'одна миллионная': 0.000001}
 adot_usual = ['десятых', 'сотых', 'тысячных', 'десятитысячных', 'стотысячных', 'миллионных']
 
-small_nums = ['ноль','один','два','три', 'четыре', 'пять', 'шесть', 'семь','восемь', 'девять']
-big_nums = ['двадцать','тридцать','сорок','пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто']
+small_nums = ['ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять']
+big_nums = ['двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто']
 
-eror_dict =  ['десять','одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать','пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'] #СПИСОК ДЛЯ ПРОВЕРКИ
+eror_dict = ['десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать',
+             'семнадцать', 'восемнадцать', 'девятнадцать']  # СПИСОК ДЛЯ ПРОВЕРКИ
 
 
-#КОНЕЦ РАБОТЫ С ДАННЫМИ
+# КОНЕЦ РАБОТЫ С ДАННЫМИ
 
-#ФУНКЦИЯ ДЕЛЕНИЯ
+# ФУНКЦИЯ ДЕЛЕНИЯ
 
 def division(numerator, denominator):
     if (numerator % denominator == 0):
@@ -53,100 +56,81 @@ def division(numerator, denominator):
                 flag = True
         return ans
 
-#ФУНКЦИЯ ДЕЛЕНИЯ
 
+# ФУНКЦИЯ ДЕЛЕНИЯ
+def usual_number(current_num):
+    match = re.search(' ', current_num[0])  # ПРОВЕРКА НА СОСТАВНОЕ ЧИСЛО 1
+    if match:
+        first_num = current_num[0].split(' ')
+        for string in eror_dict:
+            if string == current_num[0] or string == current_num[1]:
+                return -1
 
-def calc(main_str):    #ФУНКЦИЯ КАЛЬКУЛЯТОР, ЕСЛИ ВЫВОДИТСЯ ОТВЕТ "-1" - следовательно что-то сделано неверно!
+        first_flag = 0
+        second_flag = 0
+        for string in small_nums:
+            if string == current_num[1]:
+                first_flag += 1
+        for string in big_nums:
+            if string == current_num[0]:
+                second_flag += 1
 
-	if not main_str:
-		return -1
-	current_operation = 'error'
-	for string in operation_dict:   #ПОИСК ОПЕРАЦИИ В СТРОКЕ
-		match = re.search(string, main_str)
-		if match:
-			current_operation = string   #ОПЕРАЦИЯ
-	if current_operation == 'error':
-		return -1
+        if first_flag == 0 or second_flag == 0:
+            return -1
 
-	tokens = main_str.split(' ' + current_operation + ' ')   #ДЕЛИМ ЧИСЛО УДАЛЯЯ ОПЕРАЦИЮ
-	first_num, second_num = tokens[:-1], tokens[-1:]
+        first_num = digits_dict[current_num[0]] + digits_dict[current_num[1]]
+    else:
+        first_num = digits_dict[current_num[0]]
+    return first_num
 
+def calc(main_str):  # ФУНКЦИЯ КАЛЬКУЛЯТОР, ЕСЛИ ВЫВОДИТСЯ ОТВЕТ "-1" - следовательно что-то сделано неверно!
+####вСЕ ЕЩЕ НУЖНО СДЕЛАТЬ ПРОВЕРКУ ОБЯЗАТЕЛЬНО МОЛЮ СДЕЛАЙТЕ
+    if not main_str:
+        return -1
+    current_operation = 'error'
+    for string in operation_dict:  # ПОИСК ОПЕРАЦИИ В СТРОКЕ
+        match = re.search(string, main_str)
+        if match:
+            current_operation = string  # ОПЕРАЦИЯ
+    if current_operation == 'error':
+        return -1
 
+    tokens = main_str.split(' ' + current_operation + ' ')  # ДЕЛИМ ЧИСЛО УДАЛЯЯ ОПЕРАЦИЮ
+    first_num, second_num = tokens[:-1], tokens[-1:]
 
-
-	match = re.search(' ', first_num[0])    #ПРОВЕРКА НА СОСТАВНОЕ ЧИСЛО 1
-	if match:
-		first_num = first_num[0].split(' ')
-		for string in eror_dict:
-			if string == first_num[0] or string == first_num[1]:
-				return -1
-
-		first_flag = 0
-		second_flag = 0
-		for string in small_nums:
-			if string == first_num[1]:
-				first_flag += 1
-		for string in big_nums:
-			if string == first_num[0]:
-				second_flag += 1
-
-		if first_flag == 0 or second_flag == 0:
-			return -1
-
-		first_num = digits_dict[first_num[0]] + digits_dict[first_num[1]]
-	else:
-		first_num = digits_dict[first_num[0]]
-
-	match = re.search(' ', second_num[0])  #ПРОВЕРКА НА СОСТАВНОЕ ЧИСЛО 2
-	if match:
-		second_num = second_num[0].split(' ')
-		for string in eror_dict:
-			if string == second_num[0] or string == second_num[1]:
-				return -1
-
-		first_flag = 0
-		second_flag = 0
-		for string in small_nums:
-			if string == second_num[1]:
-				first_flag += 1
-		for string in big_nums:
-			if string == second_num[0]:
-				second_flag += 1
-
-		if first_flag == 0 or second_flag == 0:
-			return -1
-
-		second_num = digits_dict[second_num[0]] + digits_dict[second_num[1]]
-	else:
-		second_num= digits_dict[second_num[0]]
+    first_num = usual_number(first_num)
+    second_num = usual_number(second_num)
 
 
 
 
-	if current_operation == "плюс":
-		ans = first_num + second_num
-	elif current_operation == "умножить":
-		ans = first_num * second_num
-	elif current_operation == "минус":
-		ans = first_num - second_num
-	elif current_operation == "разделить на":
-		ans = division(first_num, second_num)
+    if first_num == -1 or second_num == -1:  #ПРОВЕРКА НА ОШИБКИ
+        return -1
 
-	ans = num2words(ans, lang='ru')
-	return ans
+    if current_operation == "плюс":
+        ans = first_num + second_num
+    elif current_operation == "умножить":
+        ans = first_num * second_num
+    elif current_operation == "минус":
+        ans = first_num - second_num
+    elif current_operation == "разделить на":
+        ans = division(first_num, second_num)
+
+    ans = num2words(ans, lang='ru')
+    return ans
 
 
 flag = True
 while flag:
-	line_main = input('Введите выражение: ')
-	line_main = line_main.lower()
+    line_main = input('Введите выражение: ')
+    line_main = line_main.lower()
 
-	ans = calc(line_main)
-	if ans != -1:
-		print('Ответ =',ans)
-		flag = False
-	else:
-		flag = True
-		print('Вы ввели неверное выражение!')
+    ans = calc(line_main)
+    if ans != -1:
+        print('Ответ =', ans)
+        flag = False
+    else:
+        flag = True
+        print('Вы ввели неверное выражение!')
 
 
